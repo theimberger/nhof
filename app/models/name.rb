d7182ad49da7cc1ca27b8a943963bf878c750431
theimberger <theimberger@gmail.com>
2017-12-18 16:22:58 -0500

    Create user and name tables and models

---
+++ app/models/name.rb
@@ -0,0 +1,8 @@
+class Name < ApplicationRecord
+  validates :name, presence: true
+
+  belongs_to :user,
+    class_name: :User,
+    foreign_key: :user_id
+    
+end
---
+++ db/migrate/20171218174510_create_names.rb
@@ -0,0 +1,11 @@
+class CreateNames < ActiveRecord::Migration[5.1]
+  def change
+    create_table :names do |t|
+      t.string :name, null: false
+      t.integer :user_id, default: :null
+      t.text :bio, null: false
+
+      t.timestamps
+    end
+  end
+end
