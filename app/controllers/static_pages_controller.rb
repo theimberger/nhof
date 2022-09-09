a9718a2f28c51ddc71cf0987e28e9b32e5602a31
Thomas Heimberger <theimberger@gmail.com>
2022-09-09 12:55:49 -0400

    Delete old project, initialize updated rails app

--- app/controllers/static_pages_controller.rb
+++
@@ -1,7 +0,0 @@
-class StaticPagesController < ApplicationController
-
-  def root
-    render 'static_pages/root'
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "static_pages#root"
-  resources :names, only: [:create, :update, :index], default: :json
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
